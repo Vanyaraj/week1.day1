b07165a1ce473535340ca997f6cca80602a00868
Vanyaraj <108991378+Vanyaraj@users.noreply.github.com>
2022-07-09 21:38:41 +0530

    Add files via upload

---
+++ Mobile.java
@@ -0,0 +1,18 @@
+package week1.Day1;
+
+public class Mobile {
+
+	public void makecall() {
+		// TODO Auto-generated method stub
+		System.out.println("simple print ");
+	}
+
+	public void sendMsg() {
+		// TODO Auto-generated method stub
+		System.out.println("simple print ");
+	}
+
+	public static void main(String[] args) {
+		System.out.println("This is my Mobile");
+	}
+}
